92262768bacfd5056f747620810611e2b8331a21
saamumc <saamu_mc@outlook.com>
2026-03-16 10:08:29 -0500

    Update AnimatedBackground.jsx

--- source/src/components/AnimatedBackground.jsx
+++ source/src/components/AnimatedBackground.jsx
@@ -19,27 +19,45 @@ const AnimatedBackground = () => {
   };
 
   const options = {
-    fullScreen: { enable: true, zIndex: -1 }, 
+    fullScreen: { enable: true, zIndex: -1 },
     particles: {
       color: { value: getParticleColor() },
       number: { value: isVoteSubmitted ? 80 : 25, density: { enable: true, area: 800 } },
       shape: { type: isVoteSubmitted ? ["circle", "star"] : "circle" },
       opacity: { value: 0.3, random: true },
       size: { value: { min: 1, max: 3 }, random: true },
-      move: { enable: true, speed: 0.8, direction: "none", outModes: { default: "out" } },
+      move: {
+        enable: true,
+        speed: 0.8,
+        direction: "none",
+        outModes: { default: "out" }
+      }
     },
-    background: { color: "transparent" }, 
+    background: { color: "transparent" }
   };
 
+  // ANIMACIÓN QUE RECORRE TODA LA PANTALLA
   const chaoticVariants = {
     animate: (custom) => ({
-      y: [0, custom.y, 0],
-      x: [0, custom.x, 0],
-      rotate: [0, custom.r || 0, 0],
+      x: [
+        "0vw",
+        `${custom.x}vw`,
+        `${-custom.x}vw`,
+        `${custom.x / 2}vw`,
+        "0vw"
+      ],
+      y: [
+        "0vh",
+        `${custom.y}vh`,
+        `${-custom.y}vh`,
+        `${custom.y / 2}vh`,
+        "0vh"
+      ],
+      rotate: [0, custom.r || 0, -(custom.r || 0), 0],
       transition: {
-        duration: custom.d,
+        duration: custom.d || 12,
         repeat: Infinity,
-        ease: "easeInOut"
+        ease: "linear"
       }
     })
   };
@@ -47,30 +65,122 @@ const AnimatedBackground = () => {
   return (
     <BackgroundWrapper>
       <Particles id="tsparticles" init={particlesInit} options={options} />
-      
+
       <FloatingElementsContainer>
-        {/* --- OSITOS GIRL (2 instancias) --- */}
-        <FloatingImg as={motion.img} src="/osito_rosa.png" custom={{ y: 25, x: 15, r: -3, d: 8 }} variants={chaoticVariants} animate="animate" style={{ bottom: '10%', left: '8%', width: '130px' }} />
 
-        {/* --- OSITOS BOY (2 instancias) --- */}
-        <FloatingImg as={motion.img} src="/osito_azul.png" custom={{ y: -20, x: -10, r: 2, d: 7 }} variants={chaoticVariants} animate="animate" style={{ top: '15%', right: '5%', width: '120px' }} />
-      
+        {/* OSITO ROSA */}
+        <FloatingImg
+          as={motion.img}
+          src="/osito_rosa.png"
+          custom={{ y: 40, x: 35, r: -5, d: 18 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ bottom: "10%", left: "8%", width: "130px" }}
+        />
 
-        {/* --- NUBES Y ESTRELLAS (10 instancias mezcladas) --- */}
-        
-        {/* Nubes */}
-        <FloatingImg as={motion.img} src="/nube_grande_1.png" custom={{ y: -10, x: 15, d: 10 }} variants={chaoticVariants} animate="animate" style={{ top: '8%', left: '5%', width: '160px', opacity: 0.6 }} />
-        <FloatingImg as={motion.img} src="/nube_grande_1.png" custom={{ y: 15, x: -20, d: 12 }} variants={chaoticVariants} animate="animate" style={{ bottom: '35%', left: '3%', width: '140px', opacity: 0.5 }} />
-        <FloatingImg as={motion.img} src="/nube_grande_1.png" custom={{ y: -18, x: -10, d: 14 }} variants={chaoticVariants} animate="animate" style={{ top: '50%', right: '25%', width: '150px', opacity: 0.4 }} />
-        <FloatingImg as={motion.img} src="/nube_grande_1.png" custom={{ y: 12, x: 25, d: 16 }} variants={chaoticVariants} animate="animate" style={{ bottom: '15%', right: '35%', width: '130px', opacity: 0.6 }} />
-        <FloatingImg as={motion.img} src="/nube_grande_1.png" custom={{ y: -15, x: 20, d: 10 }} variants={chaoticVariants} animate="animate" style={{ top: '5%', right: '35%', width: '145px', opacity: 0.5 }} />
+        {/* OSITO AZUL */}
+        <FloatingImg
+          as={motion.img}
+          src="/osito_azul.png"
+          custom={{ y: 35, x: 40, r: 6, d: 20 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "15%", right: "5%", width: "120px" }}
+        />
 
-        {/* Estrellas */}
-        <FloatingImg as={motion.img} src="/estrella_grande_1.png" custom={{ y: -10, x: 10, d: 5 }} variants={chaoticVariants} animate="animate" style={{ top: '35%', right: '12%', width: '40px' }} />
-        <FloatingImg as={motion.img} src="/estrella_grande_1.png" custom={{ y: 15, x: -10, d: 6 }} variants={chaoticVariants} animate="animate" style={{ bottom: '40%', left: '15%', width: '35px' }} />
-        <FloatingImg as={motion.img} src="/estrella_grande_1.png" custom={{ y: -8, x: -12, d: 4 }} variants={chaoticVariants} animate="animate" style={{ top: '65%', left: '40%', width: '45px' }} />
-        <FloatingImg as={motion.img} src="/estrella_grande_1.png" custom={{ y: 10, x: 20, d: 7 }} variants={chaoticVariants} animate="animate" style={{ bottom: '60%', right: '40%', width: '38px' }} />
-        <FloatingImg as={motion.img} src="/estrella_grande_1.png" custom={{ y: -15, x: -15, d: 5 }} variants={chaoticVariants} animate="animate" style={{ top: '20%', left: '50%', width: '42px' }} />
+        {/* NUBES */}
+
+        <FloatingImg
+          as={motion.img}
+          src="/nube_grande_1.png"
+          custom={{ y: 25, x: 50, d: 35 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "8%", left: "5%", width: "160px", opacity: 0.6 }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/nube_grande_1.png"
+          custom={{ y: 20, x: 45, d: 40 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ bottom: "35%", left: "3%", width: "140px", opacity: 0.5 }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/nube_grande_1.png"
+          custom={{ y: 30, x: 40, d: 45 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "50%", right: "25%", width: "150px", opacity: 0.4 }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/nube_grande_1.png"
+          custom={{ y: 28, x: 48, d: 38 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ bottom: "15%", right: "35%", width: "130px", opacity: 0.6 }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/nube_grande_1.png"
+          custom={{ y: 25, x: 42, d: 42 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "5%", right: "35%", width: "145px", opacity: 0.5 }}
+        />
+
+        {/* ESTRELLAS */}
+
+        <FloatingImg
+          as={motion.img}
+          src="/estrella_grande_1.png"
+          custom={{ y: 35, x: 60, d: 18 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "35%", right: "12%", width: "40px" }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/estrella_grande_1.png"
+          custom={{ y: 40, x: 55, d: 20 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ bottom: "40%", left: "15%", width: "35px" }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/estrella_grande_1.png"
+          custom={{ y: 45, x: 50, d: 22 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "65%", left: "40%", width: "45px" }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/estrella_grande_1.png"
+          custom={{ y: 38, x: 48, d: 21 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ bottom: "60%", right: "40%", width: "38px" }}
+        />
+
+        <FloatingImg
+          as={motion.img}
+          src="/estrella_grande_1.png"
+          custom={{ y: 42, x: 52, d: 19 }}
+          variants={chaoticVariants}
+          animate="animate"
+          style={{ top: "20%", left: "50%", width: "42px" }}
+        />
 
       </FloatingElementsContainer>
     </BackgroundWrapper>
@@ -79,13 +189,17 @@ const AnimatedBackground = () => {
 
 const BackgroundWrapper = styled.div`
   position: fixed;
-  top: 0; left: 0; width: 100%; height: 100%;
-  z-index: 0; 
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
 `;
 
 const FloatingElementsContainer = styled.div`
   position: absolute;
-  top: 0; left: 0; width: 100%; height: 100%;
+  width: 100%;
+  height: 100%;
   pointer-events: none;
 `;
 
